8e9953ef70f1696d99a14754ae960d31400c84b4
Khandelwal <abhi.khandelwal03@gmail.com>
2018-04-21 17:09:59 +0530

    folder structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const index = require('./index');
+const index = require('./src/index');
 
 const server = http.createServer(index);
 server.listen(9000,'0.0.0.0',() => console.log('Started server'));
