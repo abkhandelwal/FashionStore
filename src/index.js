365a281f79358c09af403f612881d54990c39656
Khandelwal <abhi.khandelwal03@gmail.com>
2018-04-21 17:43:33 +0530

    router-setup

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ const express =require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send('hello world');
-});
+// app.get('/',(req,res)=>{
+//     res.status(200).send('hello world');
+// });
 
+const home= require('./routes/home.routes');
+
+app.use('/',home);
 module.exports =app;
